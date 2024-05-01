70866874b4aa947aa5642915e7f0e07cec3323be
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-05-01 08:17:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+/*This is the second practice test for your exam
+tomorrow. You will have all class period to complete
+the test tomorrow, do your best, and remember to read the questions
+for the test SUPER CAREFULLY.
+Please refer to the cheatsheet as well
+You will be graded on proper naming conventions, and right or wrong answers
+half points will be provided if you get the syntax right, but do not answer
+the question correctly.
+*/
+
+/*QUESTION 1 */
+
+/* We learned how to create variables, and the different data types
+in JavaScript. In this question, i want you to create 3 variables with each data
+type, those being integer, boolean, and string.
+Please create these variables under this comment
+*/
+/*QUESTION 2 */
+/* Now inside this comment, i want you to explain the following
+what is an integer data type?
+what is an string data type?
+what is a boolean data type?
+
+PLACE ANSWER HERE:
+*/
+
+/* QUESTION 3 */
+
+/* Below you will see that a variable with the integer data type is declared
+below this comment, create a if else statement that checks to see IF THE VARIABLE
+IS GREATER THAN OR EQUAL TO 7. INSIDE THE IF STATEMENT, CONSOLE.LOG("HI")
+THEN, CREATE AN ELSE STATEMENT THAT CONSOLE.LOG("BYE")
+do this below this comment
+*/
+
+let number = 7;
+
+/*QUESTION 4 */
+
+/*Below this comment, i want you to create a for loop, set the i value to 0
+and make it so that it checks to see if i is less than 12, and then increment i
+after you create that, console.log('bye') inside the for loop
+do this below this comment
+*/
+
+
+/*QUESTION 5 */
+
+/*Below this comment, you will see a variable that is created. I want you to
+create a while loop that checks to see if the variable is less than 5. Inside 
+the for loop, i want you to decrement the variable using ++
+do this below this comment
+*/
+
+let number2 = 1;
+
+/*QUESTION 6 */
+
+/*Below this comment, i want you to create a function named hello, then, inside
+the function, i want you to console.log("hello")
+
+after you do this, i want you to call the function
+do all of this below this comment
+*/
+
+/*QUESTION 7 */
+
+/*Inside the index.html file, you will see that there is an h1 with the ID of
+goodbye. I want you to create a variable and select the h1 id using
+getElementById below this comment */
+
+
+/* QUESTION 8*/
+
+/* You just selected an h1 element, in this question, i want you to change the
+content of the h1 element using .innerHTML below this comment */
+
+
+/*QUESTION 9 */
+
+/* Inside the index.html file, you will see that there is a p class with the name
+of hi. Using getElementByClassName, i want you to create a variable and then select
+the p class using getElementByClassName below this comment
+*/
+
+
+/*QUESTION 10 */
+
+/* Inside the index.html file, you will see that there are 3 buttons with
+the class of hello. Using querySelectorAll, i want you to select all 3 buttons
+below this comment
+*/
+
+/*QUESTION 11 */
+
+/*Inside the index.html file, you will see that there is a p id that is
+named imBlue. Using querySelector, i want you to select that id. Type
+your answer below this comment */
+
+
+/*Question 12 */
+
+/*In the practice exercises, we learned about function parameters or arguments
+in this question, i want you to create a function that passes two values, a and
+b, then, inside the function, i want you to return a + b.
+
+After you do that, i want you to call the function and then pass two numbers,
+1 and 2.
+*/
+
+/*QUESTION 13(Bonus Question) */
+
+/* Once thing we learned about is arrays and how they are useful when storing
+information. Below this comment, i want you to create an array of 3 integers
+you can name the variable whatever you want */
+
+/*Question 14(Bonus Question) */
+
+/*Below, you will see an array. In this question, i want you to idientify the
+index of the array that is given */
+
+let array = [1, 2, 3, 4]
+
+/*What is the index value of the first number in the array? 
+please provide a whole number, ranging from 0 to 3
+*/
+
+/*Type Answer here */
+
+/*What is the index value of the last number in the array 
+Please provide a whole number, ranging from 0 to 3.
+*/
